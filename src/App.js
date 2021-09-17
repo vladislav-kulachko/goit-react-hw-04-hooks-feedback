dd39ad315d638008bf4e767544c65a299a611637
Vladislav Kulachko <76500070+vladislav-kulachko@users.noreply.github.com>
2021-09-17 22:39:44 +0300

    min fix

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
-import { useState } from "react";
+import {useState} from 'react';
 
-import "./App.css";
-import FeedbackOptions from "./components/Buttons/Buttons";
-import Statistics from "./components/Statistics/Statistics";
-import Section from "./components/Section/Section";
-import Notify from "./components/Notification/Notification";
+import './App.css';
+import FeedbackOptions from './components/Buttons/Buttons';
+import Statistics from './components/Statistics/Statistics';
+import Section from './components/Section/Section';
+import Notify from './components/Notification/Notification';
 
 export default function App() {
   const [state, setState] = useState(
     new Map([
-      ["good", 0],
-      ["neutral", 0],
-      ["bad", 0],
-    ])
+      ['good', 0],
+      ['neutral', 0],
+      ['bad', 0],
+    ]),
   );
 
   const [currentGood, setCurrentGood] = useState(1);
   const [currentNeutral, setCurrentNeutral] = useState(1);
   const [currentBad, setCurrentBad] = useState(1);
 
-  const handleIncrement = (currentKey) => {
-    if (currentKey === "good") {
-      setCurrentGood((s) => s + 1);
-      setState(state.set(currentKey, currentGood));
-    } else if (currentKey === "neutral") {
-      setCurrentNeutral((s) => s + 1);
-      setState(state.set(currentKey, currentNeutral));
-    } else if (currentKey === "bad") {
-      setCurrentBad((s) => s + 1);
-      setState(state.set(currentKey, currentBad));
+  const handleIncrement = currentKey => {
+    switch (currentKey) {
+      case 'good':
+        setCurrentGood(s => s + 1);
+        setState(state.set(currentKey, currentGood));
+        break;
+      case 'neutral':
+        setCurrentNeutral(s => s + 1);
+        setState(state.set(currentKey, currentNeutral));
+        break;
+      case 'bad':
+        setCurrentBad(s => s + 1);
+        setState(state.set(currentKey, currentBad));
+        break;
+      default:
+        throw new Error();
     }
   };
 
@@ -39,7 +45,7 @@ export default function App() {
         handleIncrement={handleIncrement}
       ></FeedbackOptions>
       <Statistics state={state}>
-        <Notify message={"O my God, urgently click!"}></Notify>
+        <Notify message={'O my God, urgently click!'}></Notify>
       </Statistics>
     </Section>
   );
